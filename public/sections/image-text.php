dad82b3f3a053c500799b7f16e26398e58a40384
jtzuya <jtzuya@gmail.com>
2022-12-16 23:20:41 +0800

    homepage first half done.

---
+++ public/sections/image-text.php
@@ -0,0 +1,10 @@
+<section>
+    <div class="image--text__wrapper">
+        <div class="split--text__wrapper">
+            <h2 class="heading-2">Bundle</h2>
+            <p class="body">Want it all? Get all templates in one convenient bundle.</p>
+            <a href="#" class="cta--secondary">Purchase</a>
+        </div>
+        <div class="split--image_wrapper" style="background-image: url('/public/assets/split-image/split-image1.png')"></div>
+    </div>
+</section>
