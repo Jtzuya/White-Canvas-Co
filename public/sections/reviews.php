c565ead73ad61db907db62acf7853e2d5fd4a3ef
jtzuya <jtzuya@gmail.com>
2022-12-18 20:48:11 +0800

    updates

--- public/sections/reviews.php
+++ public/sections/reviews.php
@@ -16,28 +16,30 @@
                 </div>
             </div>
 
-            <div class="review">
-                <div class="review--left--pane">
-                    <p class="client--name">Ashley L.</p>
-                    <span class="posted--date">Dec. 8, 2022</span>
-                </div>
-                <div class="review--right--pane">
-                    <div>
-                        <img src="/public/assets/review-stars.svg" alt="Rating">
+            <div class="entire--reviews">
+                <div class="review">
+                    <div class="review--left--pane">
+                        <p class="client--name">Ashley L.</p>
+                        <span class="posted--date">Dec. 8, 2022</span>
                     </div>
-                    <p class="review--description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-            </div>
-            <div class="review">
-                <div class="review--left--pane">
-                    <p class="client--name">Liela M.</p>
-                    <span class="posted--date">Dec. 2, 2022</span>
-                </div>
-                <div class="review--right--pane">
-                    <div>
-                        <img src="/public/assets/review-stars.svg" alt="Rating">
+                    <div class="review--right--pane">
+                        <div>
+                            <img src="/public/assets/review-stars.svg" alt="Rating">
+                        </div>
+                        <p class="review--description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     </div>
-                    <p class="review--description">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div class="review">
+                    <div class="review--left--pane">
+                        <p class="client--name">Liela M.</p>
+                        <span class="posted--date">Dec. 2, 2022</span>
+                    </div>
+                    <div class="review--right--pane">
+                        <div>
+                            <img src="/public/assets/review-stars.svg" alt="Rating">
+                        </div>
+                        <p class="review--description">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
                 </div>
             </div>
 
